01959862207e4ffb2c44160bed8d72bb6514666e
Grant Ammons <grant@grant.dev>
2022-02-20 16:06:31 -0500

    make a giant mess figuring this stuff out

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,39 +1,95 @@
+// see below as a reference:
+// https://docs.idew.org/video-game/project-references/phaser-coding/physics-and-collisions
 import Phaser from 'phaser';
 
 export default class Demo extends Phaser.Scene {
   balls: any[]
   graphics: any
+  leftHand: any
+  rightHand: any
 
   constructor() {
-    super('GameScene');
+    super({
+      key: "balls",
+      physics: {
+        default: 'matter',
+        matter: {
+          enableSleeping: true,
+          gravity: {
+            y: 0.2,
+          },
+          debug: {
+            showBody: true,
+            showStaticBody: true
+          }
+        }
+      }
+    });
+  }
 
-    this.balls = []
+  preload() {
+    this.load.image('ball', 'assets/aqua_ball.png')
+    this.load.image('hand', 'assets/platform.png')
   }
 
   create() {
     this.graphics = this.add.graphics()
+    this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, false, true)
 
-    for (var i = 0; i < 2000; i++) {
-      this.balls.push({
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight,
-        v: 1,
-        a: Math.random() * 2 * Math.PI,
-      });
+    for (var i = 0; i < 50; i++) {
+      const ball = this.matter.add.image(Phaser.Math.Between(100, 700), Phaser.Math.Between(-600, 0), 'ball');
+      ball.setCircle()
+      ball.setFriction(1.005);
+      ball.setBounce(1);
     }
+
+    this.leftHand = this.add.rectangle(200, 30, 100, 20, 0xffffff);
+    this.matter.add.gameObject(this.leftHand)
+
+    this.rightHand = this.add.rectangle(400, 30, 100, 20, 0xffffff);
+    this.matter.add.gameObject(this.rightHand)
+
+    this.matter.add.mouseSpring();
+
+    // tweens do not mess with physics.
+    //var timeline = this.tweens.add(
+    //  {
+    //    targets: this.leftHand,
+    //    loop: 4,
+    //    x: 600,
+    //    ease: 'Power1',
+    //    duration: 3000,
+    //    delay: 1000,
+    //    repeat: -1,
+    //    hold: 1000,
+    //    onUpdate: (tween, target) => {
+    //      console.log("target.value = ", target)
+    //      // const y = target.startY + target.value
+    //      //const dy = y - target.y
+    //      //target.y = y
+    //      target.setVelocityX(3)
+    //    }
+    //  }
+    //)
+    //timeline.play()
   }
 
   update() {
-    this.graphics.clear();
-    this.graphics.fillStyle(0x3916ff, 1);
-
-    for (let b in this.balls) {
-      var ball = this.balls[b];
-      ball.x += ball.v * Math.cos(ball.a);
-      ball.y += ball.v * Math.sin(ball.a);
-      ball.a += 0.03;
-
-      this.graphics.fillCircle(ball.x, ball.y, ball.a);
-    }
+    //console.log(this.leftHand.body.position)
+    //if (this.leftHand.y > 550 && this.leftHand.x < 600) {
+    //  this.leftHand.setPosition(this.leftHand.x + 1, this.leftHand.y)
+    //}
   }
+  //  this.graphics.clear();
+  //  this.graphics.fillStyle(0x3916ff, 1);
+
+  //  for (let b in this.balls) {
+  //    var ball = this.balls[b];
+  //    ball.x += ball.v * Math.cos(ball.a);
+  //    ball.y += ball.v * Math.sin(ball.a);
+  //    ball.a += 0.03;
+
+  //    this.graphics.fillCircle(ball.x, ball.y, ball.a);
+  //  }
+  //}
 }
